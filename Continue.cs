df02ae60f8f871637fcb294103cd1048b83b383a
Abirami301 <113671396+Abirami301@users.noreply.github.com>
2022-11-27 14:16:49 +0530

    Create Continue.cs

---
+++ Continue.cs
@@ -0,0 +1,12 @@
+public class ContinueExample  
+    {  
+      public static void Main(string[] args)  
+      {  
+         for(int i=1;i<=10;i++){    
+            if(i==5){    
+                continue;    
+            }    
+            Console.WriteLine(i);    
+        }    
+      }  
+   }  
