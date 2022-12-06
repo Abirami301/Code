c1240963f7ff78300c67fc3bec3517f55eb69836
Abirami301 <113671396+Abirami301@users.noreply.github.com>
2022-12-06 14:35:56 +0530

    Create Reverse a String Using built-in Methods.js

---
+++ Reverse a String Using built-in Methods.js
@@ -0,0 +1,17 @@
+const arrayStrings = str.split("");
+   
+    
+    const reverseArray = arrayStrings.reverse();
+ 
+    
+    const joinArray = reverseArray.join("");
+    
+    
+    return joinArray;
+}
+ 
+
+const string = prompt('Enter a string: ');
+
+const result = reverseString(string);
+console.log(result)
