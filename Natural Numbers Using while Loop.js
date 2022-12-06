1961623d4cf88a1a5401ca890d93236c6ead4627
Abirami301 <113671396+Abirami301@users.noreply.github.com>
2022-12-06 14:14:43 +0530

    Create Natural Numbers Using while Loop.js

---
+++ Natural Numbers Using while Loop.js
@@ -0,0 +1,10 @@
+const number = parseInt(prompt('Enter a positive integer: '));
+
+let sum = 0, i = 1;
+
+while(i <= number) {
+    sum += i;
+    i++;
+}
+
+console.log('The sum of natural numbers:', sum);
